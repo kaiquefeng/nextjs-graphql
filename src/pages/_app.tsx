38730d544d6adca161f38f7556a55ff2028764b2
Kaique Feng <kaiquefeng1@gmail.com>
2022-09-05 13:47:36 -0300

    feate: create boilerplate nextjs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+import { client } from "../lib/apollo";
+import "../styles/globals.scss";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default MyApp;
